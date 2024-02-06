5c9ddb64d86a94c40494c5bc13b84211a5944c26
Pritam Sharma <notpritamsharma@gmail.com>
2024-02-06 11:36:52 +0530

    first commit

---
+++ innovation_site/app/auth/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function Page() {
+  return (
+    <div className="w-screen h-screen items-center flex justify-center">
+      <Button>Login with Google</Button>
+    </div>
+  );
+}
+
+export default Page;
