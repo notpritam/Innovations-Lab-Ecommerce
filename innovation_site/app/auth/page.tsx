46f155fae895f5fd5bb342133f38766574c33396
Pritam Sharma <notpritamsharma@gmail.com>
2024-02-06 19:47:06 +0530

    Admin Panel Setup

--- innovation_site/app/auth/page.tsx
+++ innovation_site/app/auth/page.tsx
@@ -1,6 +1,16 @@
+"use client";
 import { Button } from "@/components/ui/button";
 
+//Auth Imports
+import { useSession, signIn, signOut } from "next-auth/react";
+import { redirect } from "next/navigation";
+
 function Page() {
+  // const { data: session } = useSession();
+
+  // if (session) {
+  //   redirect("/dashboard");
+  // }
   return (
     <div className="w-screen h-screen items-center flex justify-center">
       <Button>Login with Google</Button>
--- innovation_site/app/page.tsx
+++ innovation_site/app/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+
+// importing necessary functions
+import { useSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
 
 export default function Home() {
+  // extracting data from usesession as session
+  const { data: session } = useSession();
+
+  // checking if sessions exists
+  if (session) {
+    // rendering components for logged in users
+    return (
+      <>
+        <p>Welcome {session.user?.name}. Signed In As</p>
+        <p>{session.user?.email}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+
+  // rendering components for not logged in users
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between"></main>
+    <>
+      <p>Not Signed In</p>
+      <button onClick={() => signIn("google")}>Sign in with google</button>
+      <button onClick={() => signIn("github")}>Sign in with github</button>
+    </>
   );
 }
