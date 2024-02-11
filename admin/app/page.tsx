a90e2f6c7ecf86cc532f4248b93be81548e9aeb6
Pritam Sharma <notpritamsharma@gmail.com>
2024-02-11 12:17:48 +0530

    Update page.tsx to redirect to dashboard

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { useSession, signOut, getSession } from "next-auth/react";
 
-import { useSearchParams } from "next/navigation";
+import { redirect, useSearchParams } from "next/navigation";
 import Dashboard from "@/components/Tabs/Dashboard";
 import Orders from "@/components/Tabs/Orders";
 import AddProduct from "@/components/Tabs/Add_Product";
@@ -10,12 +10,8 @@ import Categories from "@/components/Tabs/Categories";
 import { getServerSession } from "next-auth";
 import Product from "@/lib/models/Product_Modal";
 import Products from "@/components/Tabs/Products";
-import TabsPage from "@/components/Tabs";
 
 export default async function Home() {
-  return (
-    <main className="flex h-screen w-screen ">
-      <TabsPage currentTab="dashboard" />
-    </main>
-  );
+  redirect("/dashboard");
+  return <main className="flex h-screen w-screen "></main>;
 }
